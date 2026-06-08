53a17d56c7c415513d717f47791f6213988763eb
xnacly <47723417+xnacly@users.noreply.github.com>
2026-06-08 16:16:44 +0200

    lsp: allow hooks behind explicit flag

--- sqleibniz/src/lib.rs
+++ sqleibniz/src/lib.rs
@@ -2,6 +2,8 @@
 pub mod error;
 /// highlight implements logic for highlighting tokens found in a string
 pub mod highlight;
+/// hooks executes user-defined lua hooks over AST and token contexts
+pub mod hooks;
 /// lev implements the levenshtein distance for all sql keywords, this is used to recommend a keyword based on a misspelled word or any
 /// unknown keyword at an arbitrary location in the source statement - mainly used at the start of a new statement
 pub mod lev;
